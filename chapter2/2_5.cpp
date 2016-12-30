0552632039ed4386c43c5e286eeff0d71c610226
anabasYU <anabas_yu@163.com>
2016-12-30 16:07:32 +0800

    Edit in 2016-12-30 4:07

---
+++ chapter2/2_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc, char const *argv[])
+{
+	cout << sizeof(wchar_t) << endl;
+	
+	return 0;
+}
---
+++ chapter2/2_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+int main(int argc, char const *argv[])
+{
+	int mouth = 9, day = 7;
+	// int myMouth = 09, myDay = 07;     this line report error.
+	int myMouth = 07, myDay = 07;
+	// float myValue = 1024f;            thie line report error.
+	cout << myValue << endl;
+
+	cout << mouth << " and " << day << endl;
+	cout << myMouth << " and " << myDay << endl;
+
+	return 0;
+}
